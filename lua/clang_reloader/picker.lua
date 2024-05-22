b4b7e3bf1a6e39bd62eb74bf237b1be08f9c9994
Filip Lobpreis <f.lobpreis@gmail.com>
2024-05-22 16:44:21 +0200

    feat: dinamic window withd
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- lua/clang_reloader/picker.lua
+++ lua/clang_reloader/picker.lua
@@ -3,12 +3,18 @@ local conf = require "telescope.config".values
 
 local telescope_reload_mapping = require("clang_reloader.mappings")
 local telecsope_reload_finder = require("clang_reloader.finder")
+local telescope = require("telescope")
 
 local M = {}
 
 --- Telescope picker changing the compilationDatabasePath where compile_commands.json is located.
 function M.reload()
-	local opts = require('telescope.themes').get_dropdown{}
+	local opts = require('telescope.themes').get_dropdown{
+		winblend = 10,
+		layout_config = {
+			width = 0.6,
+		},
+	}
 
 	pickers.new(opts, {
 		prompt_title = "compilationDatabasePath",
